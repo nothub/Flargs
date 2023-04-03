2e88010837e90bf81d6d954eb510053be221b6bd
Florian Hübner <code@hub.lol>
2023-04-03 02:29:31 +0200

    flags

--- src/main/java/hub/lol/flargs/Command.java
+++ src/main/java/hub/lol/flargs/Command.java
@@ -1,39 +1,17 @@
 package hub.lol.flargs;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
 
 public class Command extends Element implements Runnable {
-    final String name;
-    Set<Flag> flags = new HashSet<>();
-    List<String> args = new ArrayList<>();
-    Consumer<Command> func;
+    public final String name;
+    private final Set<Command> subCmds = new HashSet<>();
+    private final Consumer<Command> func;
 
-    public Command(String name) {
+    public Command(String name, Consumer<Command> func) {
         this.name = name;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public Set<Flag> flags() {
-        return flags;
-    }
-
-    public List<String> args() {
-        return args;
-    }
-
-    public void addFlag(Flag flag) {
-        this.flags.add(flag);
-    }
-
-    public void addArg(String arg) {
-        this.args.add(arg);
+        this.func = func;
     }
 
     @Override
--- src/main/java/hub/lol/flargs/Flag.java
+++ src/main/java/hub/lol/flargs/Flag.java
@@ -1,31 +1,122 @@
 package hub.lol.flargs;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public class Flag extends Element {
-    final String name;
-    final String value;
-    final Predicate<String> validator;
+public class Flag<T> extends Element {
+    private final Set<String> labels;
+    private final Function<String, T> converter;
+    private final Predicate<T> validator;
 
-    public Flag(String name, String value, Predicate<String> validator) {
-        this.name = name;
-        this.value = value;
+    private T dfault = null;
+    private T value = null;
+
+    Flag(Set<String> labels, Function<String, T> converter, Predicate<T> validator) {
+        this.labels = Collections.unmodifiableSet(labels);
+        this.converter = converter;
         this.validator = validator;
     }
 
-    public Flag(String name, String value) {
-        this(name, value, s -> true);
+    public static Flag<Boolean> boolFlag(Set<String> labels) {
+        Map<String, Boolean> lookup = Map.of(
+            "true", true,
+            "yes", true,
+            "y", true,
+
+            "false", false,
+            "no", false,
+            "n", false
+        );
+
+        Function<String, Boolean> converter = s -> {
+            if (s.isBlank()) {
+                // A bool-flag without value ( e.g. -f ) indicates true.
+                return true;
+            }
+
+            if (!lookup.containsKey(s.toLowerCase())) {
+                throw new FormatException(String.format(
+                    "Invalid value for bool-flag. Valid values (case ignored): %s",
+                    lookup.keySet().stream().sorted().collect(Collectors.joining(","))
+                ));
+            }
+
+            return lookup.get(s.toLowerCase());
+        };
+
+        return new Flag<>(labels, converter, v -> true);
     }
 
-    public String name() {
-        return name;
+    public static Flag<Integer> intFlag(Set<String> labels) {
+        return new Flag<>(labels, s -> {
+            try {
+                return Integer.parseInt(s);
+            } catch (NumberFormatException ex) {
+                throw new FormatException(ex);
+            }
+        }, v -> true);
     }
 
-    public String value() {
+    public static Flag<Float> floatFlag(Set<String> labels) {
+        return new Flag<>(labels, s -> {
+            try {
+                return Float.parseFloat(s);
+            } catch (NumberFormatException ex) {
+                throw new FormatException(ex);
+            }
+        }, v -> true);
+    }
+
+    public static Flag<String> strFlag(Set<String> labels) {
+        return new Flag<>(labels, s -> Function.<String>identity().apply(s), s -> !s.isEmpty());
+    }
+
+    public @Nullable T value() {
+        if (value == null) {
+            return dfault;
+        }
         return value;
     }
 
-    public boolean validate() {
-        return validator.test(value);
+    /**
+     * @throws FormatException
+     */
+    public void setDefault(@NotNull T value) {
+        this.dfault = value;
+        validate(this.dfault);
+    }
+
+    /**
+     * @throws FormatException
+     */
+    private void setValue(@NotNull String value) {
+        this.value = converter.apply(value);
+        validate(this.value);
+    }
+
+    private void validate(T value) {
+        if (value == null) {
+            throw new FormatException("Flag " + this.labels + " has null value.");
+        }
+        if (!validator.test(value)) {
+            throw new FormatException("Flag " + this.labels + " has invalid value.");
+        }
+    }
+
+    public static class FormatException extends RuntimeException {
+        public FormatException(String message) {
+            super(message);
+        }
+
+        public FormatException(Throwable cause) {
+            super(cause);
+        }
     }
 }
